dea777f3a9e133ab1b0aaa9f53ae26dba1b0175e
Francis Chomba <francischomba@gmail.com>
2023-08-16 13:37:05 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - make the alphabet
- *
- * Return:void
+ * print_alphabet - Entry point
+ * Return:0
  */
 
 void print_alphabet(void)
 {
-	char ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(ch);
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
 	_putchar('\n');
+
 }
